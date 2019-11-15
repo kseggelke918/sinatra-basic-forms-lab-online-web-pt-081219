6412da447c49e30c9d224ef2aaaf671b3e398114
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-15 17:32:59 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,7 +1,10 @@
 class Puppy 
+  attr_reader :name, :breed, :months_old
   
-  def initialize
-    
+  def initialize(name, breed, months_old)
+    @name = name 
+    @breed = breed
+    @months_old = months_old
   end 
   
 end 
